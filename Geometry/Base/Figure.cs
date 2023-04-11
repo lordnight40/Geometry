6195cb8c1f39c77828ddc88cb0f41212433d2d82
Kirill Bugaev <lordnight40@gmail.com>
2023-04-11 22:07:44 +0300

    Комментарии

--- Geometry/Base/Figure.cs
+++ Geometry/Base/Figure.cs
@@ -1,6 +1,17 @@
 ﻿namespace Geometry.Base;
 
+/// <summary>
+/// Базовый абстрактный класс фигуры.
+/// <remarks>Делаем так, потому что между дочерними классами и этим будет отношение "является".</remarks>
+/// </summary>
 public abstract class Figure
 {
-    public abstract double CalculateSquare();
+    /// <summary>
+    /// Метод вычисления площади.
+    /// </summary>
+    /// <remarks>Можно было бы добавить этот метод через интерфейс (казалось бы отношение "поддерживает"),
+    /// но площадь является атрибутом любой фигуры.
+    /// </remarks>
+    /// <returns>Площадь фигуры.</returns>
+    public abstract double CalculateArea();
 }
