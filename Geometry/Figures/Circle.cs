1a50b2d958841bcf1e6a4df80aa71cc8a57115bb
Kirill Bugaev <lordnight40@gmail.com>
2023-04-11 22:15:21 +0300

    Круг

---
+++ Geometry/Figures/Circle.cs
@@ -0,0 +1,23 @@
+﻿using Geometry.Base;
+
+namespace Geometry.Figures;
+
+public class Circle : Figure
+{
+    public double Radius { get; }
+
+    public Circle(double radius)
+    {
+        if (radius <= 0)
+        {
+            throw new ArgumentException("Получено некорректное значение радиуса. Радиус должен быть > 0");
+        }
+
+        this.Radius = radius;
+    }
+    
+    public override double CalculateArea()
+    {
+        return Math.PI * Math.Pow(Radius, 2);
+    }
+}
